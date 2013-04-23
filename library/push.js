25b843cecdd2b6dbc27a9ab6e712d3f4b088f557
Fernando Rodríguez Sela <frsela@tid.es>
2013-04-23 19:00:53 +0200

    wakeup host only IP address (no domain name)

--- library/push.js
+++ library/push.js
@@ -215,7 +215,7 @@ _Push.prototype = {
       ssl: true,
       keepalive: 60000,
       wakeup_enabled: false,
-      wakeup_host: 'localhost',
+      wakeup_host: '127.0.0.1',
       wakeup_port: 8080,
       wakeup_protocol: 'tcp',
       wakeup_mcc: '214',
